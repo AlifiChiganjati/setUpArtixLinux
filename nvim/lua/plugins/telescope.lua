7d924044eac7a9f9573736419686fd8324495f55
alifichiganjati <alifichiganjati@gmail.com>
2023-10-31 20:14:18 +0700

    fix: add terminal

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,13 +3,6 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
   },
   keys = {
       -- add a keymap to browse plugin files
