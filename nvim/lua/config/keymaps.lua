8be05ed4b1dd51f499e55a5c8e131ec970b9608b
alifichiganjati <alifichiganjati@gmail.com>
2023-10-31 19:43:47 +0700

    fix: keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local opts = { noremap = true, silent = true }
+local map = vim.keymap.set -- for conciseness
+-- Press jk fast to exit insert mode
+map("i", "jk", "<ESC>", opts)
+map("i", "kj", "<ESC>", opts)
+
+--emoji
+map("i", "<c-i>", "<cmd>IconPickerInsert emoji<cr>", opts)
